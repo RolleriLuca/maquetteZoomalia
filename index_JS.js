becfe8e3755d44ed4d8ced6a568568ae953f0d4b
RolleriLuca <lrolleri3053@gmail.com>
2021-01-22 16:33:56 +0100

    [js] add slider

--- index_JS.js
+++ index_JS.js
@@ -1,4 +1,4 @@
-// ---------------------------------------------DECLARATION DES VARIABLES---------------------------------------------
+// ---------------------------------------------GESION DU MENU DEROULANT---------------------------------------------
 
 let scrollingMenuTop = document.getElementById('scrollingMenu_top');
 let scrollingList = document.getElementById('scrollingList');
@@ -27,4 +27,53 @@ function scrolllingMenuApparition()
     console.log(nbClicks);
 }
 
+// ---------------------------------------------GESION DU MENU DEROULANT---------------------------------------------
 
+const slides = document.getElementById('slides');
+const prevButton = document.getElementById('prev');
+const nextButton = document.getElementById('next');
+// const limit = document.querySelectorAll('.perso').length;
+const limit = document.getElementsByClassName('perso').length;
+let spanMax = document.getElementById('slides').offsetWidth;
+console.log(limit);
+console.log(spanMax);
+let current = 0;
+
+prevButton.addEventListener('click', prev);
+nextButton.addEventListener('click', next);
+
+
+spanMax = limit * 203;
+
+slides.style.width = `${spanMax}px`;
+
+
+function prev()
+{
+    if (current > 0)
+    {
+        current--;
+        console.log(current);
+        move()
+    }  
+}
+
+function next()
+{
+    if (current < limit - 1)
+    {
+        current++;
+        console.log(current);
+        move()
+    }
+    
+}
+
+function move()
+{
+    const transform = -203 * current;
+    slides.style.transform = `translateX(${transform}px)`;
+
+    prevButton.style.display = current === 0 ? "none" : "block";
+    nextButton.style.display = current === limit - 1 ? "none" : "block";
+}
